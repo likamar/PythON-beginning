51727688c05ac8480851abb2ee8476b1f2f17c7f
likamar <lika.marcin@gmail.com>
2023-08-27 21:37:39 +0200

    add __name__ var condition

--- mod_2/Klasy i obiekty/zadanie_1.py
+++ mod_2/Klasy i obiekty/zadanie_1.py
@@ -14,29 +14,30 @@ class Potato:
     pass
 
 
-apple_1 = Apple()
-apple_2 = Apple()
-apple_3 = Apple()
-potato_1 = Potato()
-potato_2 = Potato()
+if __name__ == "__main__":
+    apple_1 = Apple()
+    apple_2 = Apple()
+    apple_3 = Apple()
+    potato_1 = Potato()
+    potato_2 = Potato()
 
-print(f"First apple class: {type(apple_1)}")
-print(f"Second apple class: {type(apple_2)}")
-print(f"Third apple class: {type(apple_3)}")
-print(f"First potato class: {type(potato_1)}")
-print(f"Second potato class: {type(potato_2)}")
+    print(f"First apple class: {type(apple_1)}")
+    print(f"Second apple class: {type(apple_2)}")
+    print(f"Third apple class: {type(apple_3)}")
+    print(f"First potato class: {type(potato_1)}")
+    print(f"Second potato class: {type(potato_2)}")
 
-orders = []
+    orders = []
 
-for order in range(5):
-    orders.append(Order())
+    for order in range(5):
+        orders.append(Order())
 
-print(orders)
+    print(orders)
 
-products = {
-    "milk": Product(),
-    "bread": Product(),
-    "coca-cola": Product()
-}
+    products = {
+        "milk": Product(),
+        "bread": Product(),
+        "coca-cola": Product()
+    }
 
-print(products)
+    print(products)

4dd3474a9a11ac6be4524f058c37612fec787223
likamar <lika.marcin@gmail.com>
2023-08-27 23:02:52 +0200

    add constructors

--- mod_2/Klasy i obiekty/zadanie_1.py
+++ mod_2/Klasy i obiekty/zadanie_1.py
@@ -1,43 +1,66 @@
 class Product:
-    pass
+    def __init__(self, name, category, price):
+        self.name = name
+        self.category = category
+        self.price = price
 
 
 class Order:
-    pass
+    def __init__(self, first_name, last_name, products=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.products = products
+        self.total_price = 0
+        for product in products:
+            self.total_price += product.price
 
 
 class Apple:
-    pass
+    def __init__(self, grade_name, size, price):
+        self.grade_name = grade_name
+        self.size = size
+        self.price = price
 
 
 class Potato:
-    pass
+    def __init__(self, grade_name, size, price):
+        self.grade_name = grade_name
+        self.size = size
+        self.price = price
 
 
 if __name__ == "__main__":
-    apple_1 = Apple()
-    apple_2 = Apple()
-    apple_3 = Apple()
-    potato_1 = Potato()
-    potato_2 = Potato()
+    apple_jonagold = Apple("Jonagold", "L", 3.50)
+    apple_champion = Apple("Champion", "XL", 3.99)
 
-    print(f"First apple class: {type(apple_1)}")
-    print(f"Second apple class: {type(apple_2)}")
-    print(f"Third apple class: {type(apple_3)}")
-    print(f"First potato class: {type(potato_1)}")
-    print(f"Second potato class: {type(potato_2)}")
+    print(f"Jonagold name: {apple_jonagold.grade_name}")
+    print(f"Jonagold size: {apple_jonagold.size}")
+    print(f"Jonagold price: {apple_jonagold.price}")
+    print(f"Champion name: {apple_champion.grade_name}")
+    print(f"Champion size: {apple_champion.size}")
+    print(f"Champion price: {apple_champion.price}")
+
+    products = [
+        Product("Milk", "dairy", 3.50),
+        Product("Bread", "breadstuff", 4.50),
+        Product("Coca-Cola", "beverages", 7.99)
+    ]
 
     orders = []
 
-    for order in range(5):
-        orders.append(Order())
+    order_1 = Order("Kamil", "Kowalski", products)
+    order_2 = Order("Maja", "Walczak", products)
 
-    print(orders)
+    orders.append(order_1)
+    orders.append(order_2)
 
-    products = {
-        "milk": Product(),
-        "bread": Product(),
-        "coca-cola": Product()
-    }
+    def print_product_details(product):
+        print(product.name, product.category, product.price, sep=",")
 
-    print(products)
+    def print_order_details(order):
+        print(order.first_name, order.last_name, order.total_price, sep=",")
+        for product in order.products:
+            print_product_details(product)
+
+    for order in orders:
+        print_order_details(order)
